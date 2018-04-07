5c8d418e347b779331f0aed988e767cd024e9a20
Dam Minh Tien <damminhtienchl@gmail.com>
2018-04-07 17:03:53 +0700

    Revert "Giao dien"

--- resources/views/admin/gioithieu.blade.php
+++ resources/views/admin/gioithieu.blade.php
@@ -1,18 +1,6 @@
 @extends('admin.layouts.index')
 @section('content')
-<div id="root">
-	
+<div>
+	<h1>HELLO WORLD!!!</h1>
 </div>
-@endsection
-@section('script')
-<script src="https://cdnjs.cloudflare.com/ajax/libs/marked/0.3.6/marked.js" type="text/javascript" charset="utf-8"></script>
-<script>
-$('document').ready(()=>{
-	$.get('https://raw.githubusercontent.com/damminhtien/bkfa/master/readme.md', function(data) {
-			chuoi = marked(data);
-		console.log(chuoi);
-		$('#root').html(chuoi);
-	}, 'text');
-})
-</script>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,86 +14,38 @@
 use App\vien;
 use App\mon;
 
-Route::group(['prefix'=>'admin'],function(){
+
 	Route::get('/', function(){
 		return view('admin.gioithieu');
 	});
-	
-	Route::group(['prefix'=>'ajax'],function(){
-		Route::get('getmonby{idvien}','ajaxController@getMonByVien');
+
+
+
+	Route::group(['prefix'=>'admin'],function(){
+		Route::group(['prefix'=>'vien'],function(){
+			Route::get('danhsach','vienController@getDanhSach');
+
+			Route::post('them','vienController@postThem');
+
+			Route::post('sua/{idvien}','vienController@postSua');
+			
+			Route::get('xoa/{idvien}','vienController@getXoa');
+		});
+
+		Route::group(['prefix'=>'slide'],function(){
+			Route::get('danhsach','slideController@getDanhSach');
+
+			Route::get('them','slideController@getThem');
+			Route::post('them','slideController@postThem');
+
+			Route::get('sua/{idslide}','slideController@getSua');
+			Route::post('sua/{idslide}','slideController@postSua');
+			
+			Route::get('xoa/{idslide}','slideController@getXoa');
+		});
+
+		Route::group(['prefix'=>'ajax'],function(){
+			Route::get('getmonby{idvien}','ajaxController@getMonByVien');
+		});
 	});
-
-	Route::group(['prefix'=>'vien'],function(){
-		Route::get('danhsach','vienController@getDanhSach');
-
-		Route::post('them','vienController@postThem');
-
-		Route::post('sua/{idvien}','vienController@postSua');
-
-		Route::get('xoa/{idvien}','vienController@getXoa');
-	});
-
-	Route::group(['prefix'=>'mon'],function(){
-		Route::get('danhsach','monController@getDanhSach');
-
-		Route::get('them','monController@getThem');
-		Route::post('them','monController@postThem');
-
-		Route::get('sua/{idmon}','monController@getSua');
-		Route::post('sua/{idmon}','monController@postSua');
-
-		Route::get('xoa/{idmon}','monController@getXoa');
-	});
-
-	Route::group(['prefix'=>'slide'],function(){
-		Route::get('danhsach','slideController@getDanhSach');
-
-		Route::get('them','slideController@getThem');
-		Route::post('them','slideController@postThem');
-
-		Route::get('sua/{idslide}','slideController@getSua');
-		Route::post('sua/{idslide}','slideController@postSua');
-
-		Route::get('xoa/{idslide}','slideController@getXoa');
-	});
-
-	Route::group(['prefix'=>'tintuc'],function(){
-		Route::get('danhsach','tintucController@getDanhSach');
-
-		Route::get('them','tintucController@getThem');
-		Route::post('them','tintucController@postThem');
-
-		Route::get('sua/{idtintuc}','tintucController@getSua');
-		Route::post('sua/{idtintuc}','tintucController@postSua');
-
-		Route::get('xoa/{idtintuc}','tintucController@getXoa');
-	});
-
-	Route::group(['prefix'=>'dethi'],function(){
-		Route::get('danhsach','dethiController@getDanhSach');
-
-		Route::get('them','dethiController@getThem');
-		Route::post('them','dethiController@postThem');
-
-		Route::get('sua/{iddethi}','dethiController@getSua');
-		Route::post('sua/{iddethi}','dethiController@postSua');
-
-		Route::get('xoa/{iddethi}','dethiController@getXoa');
-	});
-
-	Route::group(['prefix'=>'user'],function(){
-		
-		Route::get('danhsach','userController@getDanhSach');
-
-
-		Route::get('sua/{iduser}','userController@getSua');
-		Route::post('sua/{iduser}','userController@postSua');
-
-		Route::get('them','userController@getThem');
-		Route::post('them','userController@postThem');
-
-		Route::get('xoa/{id}','userController@getXoa');
-	});
-});
-
 
